41831fbc43ce29c04c370697d7a96af3797f840e
mehdi <mehdilahrach89@gmail.com>
2023-06-18 04:48:21 -0700

    putchar ascii

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0 success
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
