ecbbc730e26c022fc171e87cd4ec0ea64a8fe364
mehdi <mehdilahrach89@gmail.com>
2023-06-18 05:34:15 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0 when success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96 ; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
