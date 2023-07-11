f33d3d5483747b0824e4c41d0698255685bd7900
mehdi <mehdilahrach89@gmail.com>
2023-07-11 12:15:11 -0700

    edits

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,12 +17,12 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	while (str[len] == '\0')
+	while (str[len] != '\0')
 		len++;
 
 	copy = (char *)malloc((sizeof(char) * len) + 1);
 
-	for(i = 0; i <len; i++)
+	for (i = 0; i < len; i++)
 		copy[i] = str[i];
 	copy[len] = '\0';
 
